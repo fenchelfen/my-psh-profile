4b96c8ff53018993f86d2764ad725d218223ca74
fenchelfen <m.lyamets@innopolis.ru>
2020-08-11 08:46:03 -0700

    Add profile file

---
+++ profile.ps1
@@ -0,0 +1,5 @@
+$env:pshrc="C:\Users\ff\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+$env:pshprofile="C:\Users\ff\Documents\WindowsPowerShell\profile.ps1"
+$env:PATH+=";C:\Program Files\Git\bin"
+$env:vcvars12="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+$env:vcvars19="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
